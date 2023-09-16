da3724ebd44560508a5ea9554c7a6828b5d435a9
MuhammedBilalA <120563391+MuhammedBilalA@users.noreply.github.com>
2023-09-16 11:17:57 +0530

    First commit

---
+++ widgets-unique-codes.dart
@@ -0,0 +1,67 @@
+
+// volume slider  
+
+Expanded(
+                      child: Slider(
+                        inactiveColor: Colors.transparent,
+                        value: _volume,
+                        min: 0.0,
+                        max: 100.0,
+                        divisions: 10,
+                        label: '${(_volume).round()}',
+                        onChanged: _isPlayerReady
+                            ? (value) {
+                                setState(() {
+                                  _volume = value;
+                                });
+                                _controller.setVolume(_volume.round());
+                              }
+                            : null,
+                      ),
+                    ),
+
+
+
+
+// animated container color change  
+
+
+AnimatedContainer(
+                    duration: const Duration(milliseconds: 800),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: _getStateColor(_playerState),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      _playerState.toString(),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+
+  Color _getStateColor(PlayerState state) {
+    switch (state) {
+      case PlayerState.unknown:
+        return Colors.grey[700]!;
+      case PlayerState.unStarted:
+        return Colors.pink;
+      case PlayerState.ended:
+        return Colors.red;
+      case PlayerState.playing:
+        return Colors.blueAccent;
+      case PlayerState.paused:
+        return Colors.orange;
+      case PlayerState.buffering:
+        return Colors.yellow;
+      case PlayerState.cued:
+        return Colors.blue[900]!;
+      default:
+        return Colors.blue;
+    }
+  }
+
+ FocusScope.of(context).requestFocus(FocusNode()); // for remove the keyboard
